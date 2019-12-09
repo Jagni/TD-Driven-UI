a28bcb3629e6fec914aee4792691f22844bc724e
Jagni Dasa <jagnidasa@gmail.com>
2019-12-09 11:58:32 -0300

    Using buildInfoButton in input widgets

--- lib/default_widgets/form/input/number/wrapper.dart
+++ lib/default_widgets/form/input/number/wrapper.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:td_driven_ui/default_widgets/form/input/number/specifier.dart';
-import 'package:td_driven_ui/thing_ui_models/thing_ui_models.dart';
-
+import 'package:td_driven_ui/utils/info_dialog.dart';
 
 class TdUiNumberInputWrapper extends StatelessWidget {
   final controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    var children = [
+        Align(alignment: Alignment.center, child: TdUiNumberInputSpecifier()),
+      ];
+    final infoButton = buildInfoButton(context);
+    if (infoButton != null){
+      children.add(Align(alignment: Alignment.topRight, child: infoButton));
+    }
     return ListenableProvider<TextEditingController>.value(
-        child: TdUiNumberInputSpecifier(),
-        value: controller,
-      );
+      child: Stack(children: children),
+      value: controller,
+    );
   }
+
 }
